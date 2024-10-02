d9f05648c87b9894b6bf0dc3be069a025729bf4c
fireflyshen <59105078+fireflyshen@users.noreply.github.com>
2024-10-03 02:11:15 +0800

    适配移动端

--- src/component/Main/WorkExperience/WorkExperience.jsx
+++ src/component/Main/WorkExperience/WorkExperience.jsx
@@ -1,23 +1,51 @@
+import { useEffect, useState } from 'react'
 import person from '../../../../personInfo.json'
 import Title from '../../common/Title'
 import './WorkExperience.css'
-
+import { Popover } from 'antd'
 
 export default function WorkExperience() {
     const { workExperience } = person
-    return (
+    const [isHovered, setIsHovered] = useState(null);
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 750);
 
-        <div className="experience">     
-            <Title partTitle="工作经历"/>
+    var handleHover = index => {
+        setIsHovered(index);
+    }
+
+    var handlerLeave = () => {
+        setIsHovered(null);
+    }
+
+
+    const handleResize = () => {
+        setIsSmallScreen(window.innerWidth < 750);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+
+    return (
+        <div className="experience">
+            <Title partTitle="工作经历" />
             <div className="timeline">
                 {
                     workExperience.map((item, index) => {
                         return (
-                            <div className="experienceItem" key={index}>
-                                <div className="experienceDate">{item.startDate}</div>
-                                <div className='experienceCircle'>
-                                </div>
-                                <div className='experienceContent'>
+                            <div className={`experienceItem ${index == isHovered ? `hover` : ``}`} key={index}>
+                                {!isSmallScreen && <div className="experienceDate">{item.startDate}</div>}
+                                {
+                                    isSmallScreen ? (<Popover content="" title={item.startDate}>
+                                        <div className='experienceCircle' >
+                                        </div>
+                                    </Popover>) : (<div className='experienceCircle' />)
+                                }
+                                <div className='experienceContent' onMouseEnter={() => handleHover(index)} onMouseLeave={handlerLeave}>
                                     <div className="experienceContent">
                                         <div>公司：{item.company}</div>
                                         <div>年限：{item.year}</div>
