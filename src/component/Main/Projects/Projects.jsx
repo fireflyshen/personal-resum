2af3fff4e5d442aca3228fc3a980cc88c1e5480f
fireflyshen <59105078+fireflyshen@users.noreply.github.com>
2024-10-02 11:45:29 +0800

    a

--- src/component/Main/Projects/Projects.jsx
+++ src/component/Main/Projects/Projects.jsx
@@ -3,12 +3,18 @@ import Title from "../../common/Title";
 export default function Projects() {
   return (
     <>
-        <Title partTitle="个人项目"/>
-        <div>
-
-          
+      <Title partTitle="个人项目" />
+      <div>
+        <div className="projectItem">
+          <div className="projectContent">
+            <div>项目名称：React项目</div>
+            <div>项目描述：使用React框架完成的项目</div>
+            <div>项目技术：React、React-Router、Antd、Axios</div>
+            <div>项目地址：<a href=""></a>
+            </div>
+          </div>
         </div>
+      </div>
     </>
-
   );
 }
